83f87e48ae4a25cd3aaa4aad19f3d1e991c733e3
dhananjayadr <vishwasdhananjaya@gmail.com>
2021-09-26 18:29:06 +0530

    changed logic

--- solutions/4.py
+++ solutions/4.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
-result = []
-for x in range(100, 1000):
-    for y in range(100, 1000):
+result = 0
+for x in range(999, 100, -1):
+    for y in range(999, 100, -1):
         temp = x * y
-        if str(temp) == str(temp)[::-1]:
-            result.append(temp)
-print(max(result))
+        if str(temp) == str(temp)[::-1] and temp > result:
+            result = temp
+            break
+
+print(result)
 
